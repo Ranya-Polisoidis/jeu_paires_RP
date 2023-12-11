7a045389abb452604acdaba2f70e2c8b076f4a91
Ranya <ranyapolisoidis2001@gmail.com>
2023-12-11 20:27:29 +0100

    Début main.js: récupération de ma class: devantDeCarteFacile + création d'un tableau pour mes diff images + ça fonction qui permet de mélanger le tableau des images de manière aléatoire et attribuer les images aux éléments HTML

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,21 @@
 
 
+
+//###################################################################################################################################################################################
+
+let devantDeCarteFacileAll = document.querySelectorAll(".devantDeCarteFacile")
+
+let tabRandomCarte = ["./public/img/img1.difficulterFacile.jpeg", "./public/img/img2.difficulterFacile.jpeg", "./public/img/img3.difficulterFacile.jpeg", "./public/img/img1.difficulterFacile.jpeg", "./public/img/img2.difficulterFacile.jpeg", "./public/img/img3.difficulterFacile.jpeg"]
+
+tabRandomCarte.sort(() => Math.random() - 0.5);
+
+for (let i = 0; i < devantDeCarteFacileAll.length; i++) {
+    devantDeCarteFacileAll[i].src = tabRandomCarte[i];
+}
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
