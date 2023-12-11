21867ae042f595aed5f7c00ac0bc31b5cecdc983
Ranya <ranyapolisoidis2001@gmail.com>
2023-12-11 23:12:39 +0100

    Création de ma partie : difficulté facile --> création de : compteur, stop, mes trois tableaux (tabStockerSrc,tabStockerVariable,tabStockerDos), fonction : quand je clique dans mon btn facile, toutes les cartes de dos sont affichées, à chaque fois que je vais cliquer sur l'une des cartes mtn visibles, il est à l'état stop donc je mets son résultat en mettant son compteur ++ puis je lui donne une limite de 2 et je fais ensuite une comparaison des deux cartes si elles sont les Ensuite, je réinitialise tout : Je remets le tableau à 0 et le compteur à 0. Je rechange les valeurs stop pour qui refais le processus.

--- public/js/main.js
+++ public/js/main.js
@@ -32,4 +32,160 @@ console.log(btnFacile);
 
 // ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+let compteur=0
+let stop=false
 
+let tabStockerSrc=[]
+let tabStockerVariable=[]
+let tabStockerDos=[]
+
+btnFacile.addEventListener("click", function () {
+
+    dosDeCarteFacileAll.forEach(element => {
+        element.style.display="flex"
+    });
+
+    dosDeCarteFacileAll.forEach(element => {
+        element.addEventListener("click", function () {
+
+            if(stop==false){
+                element.style.display="none"
+                element.nextElementSibling.style.display="flex"
+
+
+
+                let stockerSrc= element.nextElementSibling.src
+                tabStockerSrc.push(stockerSrc)
+                console.log(tabStockerSrc[0]);
+                console.log(tabStockerSrc[1]);
+                console.log(`------`);
+
+                let stockerVariable=element.nextElementSibling
+                tabStockerVariable.push(stockerVariable)
+                console.log(tabStockerVariable[0]);
+                console.log(tabStockerVariable[1]);
+                console.log(`------`);
+
+
+
+                let stockerDos=element
+                tabStockerDos.push(stockerDos)
+                console.log(tabStockerDos[0]);
+                console.log(tabStockerDos[1]);
+                console.log(`------`);
+
+
+    
+                compteur++
+            }
+
+            if(compteur==2){
+                stop=true
+
+
+
+                // base des bases ici 
+                // if(tabStockerSrc[0]==tabStockerSrc[1]){
+                //     // element.nextElementSibling.style.border="green 2px solid"
+                //     tabStockerVariable[0].style.border="green 2px solid"
+                //     tabStockerVariable[1].style.border="green 2px solid"
+                // } else {
+
+                //     tabStockerVariable[0].style.border="red 2px solid"
+                //     tabStockerVariable[1].style.border="red 2px solid"
+                // 
+                if(tabStockerSrc[0]==tabStockerSrc[1]){
+                    // element.nextElementSibling.style.border="green 2px solid"
+                    tabStockerVariable[0].style.border="green 2px solid"
+                    tabStockerVariable[1].style.border="green 2px solid"
+                } else {
+                    tabStockerVariable[0].style.border="red 2px solid"
+                    tabStockerVariable[1].style.border="red 2px solid"
+
+                    // setTimeout(() => {
+                    //     tabStockerVariable[0].src="./public/img/dosDeCarte.jpeg"
+                    //     tabStockerVariable[1].src="./public/img/dosDeCarte.jpeg"
+                    // }, "1000");
+                    // if(tabStockerVariable[0].style.border=="red 2px solid" && tabStockerVariable[1].style.border=="red 2px solid"){
+                    //     tabStockerVariable[0].src="./public/img/dosDeCarte.jpeg"
+                    //     tabStockerVariable[1].src="./public/img/dosDeCarte.jpeg"
+                    // }
+
+                    
+
+
+                    // console.log(`A partir de en dessous`);
+                    // tabStockerVariable[0].style.display="none"
+                    // console.log(tabStockerVariable[0]);
+                    // tabStockerVariable[1].style.display="none"
+                    // console.log(tabStockerVariable[1]);
+                    // tabStockerDos[0].style.display="flex"
+                    // console.log(tabStockerDos[0]);
+                    // tabStockerDos[1].style.display="flex"
+                    // console.log(tabStockerDos[1]);
+
+                    // console.log(`A partir de en dessous`);
+                    // tabStockerVariable[0].style.display="none"
+                    // console.log(tabStockerVariable[0]);
+                    // tabStockerVariable[1].style.display="none"
+                    // console.log(tabStockerVariable[1]);
+                    // tabStockerDos[0].style.display="flex"
+                    // console.log(tabStockerDos[0]);
+                    // tabStockerDos[1].style.display="flex"
+                    // console.log(tabStockerDos[1]);
+
+                    // console.log(`A partir de en dessous`);
+                    // tabStockerVariable[0].style.display="none"
+                    // console.log(tabStockerVariable[0]);
+                    // tabStockerVariable[1].style.display="none"
+                    // console.log(tabStockerVariable[1]);
+                    // tabStockerDos[0].style.display="flex"
+                    // console.log(tabStockerDos[0]);
+                    // tabStockerDos[1].style.display="flex"
+                    // console.log(tabStockerDos[1]);
+
+
+                    // // teste 1
+                    // tabStockerVariable[0].setAttribute("class", "none")
+                    // tabStockerVariable[1].setAttribute("class", "none")
+                    // console.log(tabStockerVariable[0]);
+                    // console.log(tabStockerVariable[1]);
+
+                    // tabStockerDos[0].setAttribute("class", "flex")
+                    // tabStockerDos[1].setAttribute("class", "flex")
+                    // console.log(tabStockerDos[0]);
+                    // console.log(tabStockerDos[1]);
+                    // // fin teste 1
+
+
+
+                    // if(tabStockerVariable[0].style.border=="red 2px solid" && tabStockerVariable[1].style.border=="red 2px solid"){
+                    //     tabStockerVariable[0].style.display="none"
+                    //     tabStockerVariable[1].style.display="none"
+    
+                    //     tabStockerDos[0].style.display="flex"
+                    //     tabStockerDos[1].style.display="flex"
+                    // }
+                } 
+                tabStockerSrc=[]
+                tabStockerVariable=[]
+                tabStockerDos=[]
+                stop=false
+                compteur = 0
+
+
+
+            }
+
+    
+
+        })
+
+    });
+
+})
+
+
+                    // setTimeout(() => {
+                    //     alert(`je fct ok ?`)
+                    // }, "1000");
