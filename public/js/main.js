5f97cd68b56e89f3ee6cd0e48672aa8a093b3974
Ranya <ranyapolisoidis2001@gmail.com>
2023-12-12 18:52:53 +0100

    // ajout d'un input et son bouton go qui affichera les éléments du tableauDesScores ainsi que le choix grâce au boutons des différents niveau de jeux (ici le facile fct) qui une fois choisis disparais pour pas réutiliser mais quand on termine la partie on peu rejouer ou choisir une autre niveau de difficulter. Séparation des deux addEventListener (car provoquais des soucis) + nouvelle condition si on arrive à un total de 6 carte identique grâce a un compteur pour retourner les cartes. Ajout d'un de deux setTimeout pour voir les effets désirers et prendre le temps de donner une limite pour obtenir un arrêt au bout de 2 choix, ainsi qu'au retournement des cartes

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,5 @@
 
 
-
 //###################################################################################################################################################################################
 
 let devantDeCarteFacileAll = document.querySelectorAll(".devantDeCarteFacile")
@@ -15,177 +14,154 @@ for (let i = 0; i < devantDeCarteFacileAll.length; i++) {
 
 // ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-
 let dosDeCarteFacileAll = document.querySelectorAll(".dosDeCarteFacile")
+
 
 let btnAll = document.querySelectorAll("button")
 
-let btnFacile = btnAll[0]
-console.log(btnFacile);
+let btnGO = btnAll[0]
 
-// // temporairement pas utiliser
-// let btnNormal=btnAll[1]
+let input1 = document.querySelector("input")
+// console.log(input1);
+let h3_1 = document.querySelector("h3")
+// console.log(h3_1);
+let h1_2 = document.querySelectorAll("h1")[1]
+// console.log(h1_2);
+
+let difficuterDeJeu = document.querySelector(".difficuterDeJeu")
+
+
+btnGO.addEventListener("click", function () {
+    let vP = input1.value
+    h3_1.innerHTML = vP;
+    h3_1.style.color="rgb(170, 115, 48)"
+    h3_1.style.fontSize="50px"
+
+    h1_2.style.display = "flex"
+    difficuterDeJeu.style.display = "flex"
+})
+
+let btnFacile = btnAll[1]
+// console.log(btnFacile);
+let btnNormal = btnAll[2]
 // console.log(btnNormal);
-// let btnDifficile=btnAll[2]
+let btnDifficile = btnAll[3]
 // console.log(btnDifficile);
-// // 
 
 // ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-let compteur=0
-let stop=false
+let compteur = 0
+let stop = false
 
-let tabStockerSrc=[]
-let tabStockerVariable=[]
-let tabStockerDos=[]
+let tabStockerSrc = []
+let tabStockerVariable = []
+
+let compteurRécup6=0
+
 
 btnFacile.addEventListener("click", function () {
 
+    difficuterDeJeu.style.display = "none"
+    console.log("testtt");
+    console.log(tabStockerVariable);
+    console.log(tabStockerSrc);
     dosDeCarteFacileAll.forEach(element => {
-        element.style.display="flex"
+        element.style.display = "flex"
     });
-
-    dosDeCarteFacileAll.forEach(element => {
-        element.addEventListener("click", function () {
-
-            if(stop==false){
-                element.style.display="none"
-                element.nextElementSibling.style.display="flex"
-
-
-
-                let stockerSrc= element.nextElementSibling.src
-                tabStockerSrc.push(stockerSrc)
-                console.log(tabStockerSrc[0]);
-                console.log(tabStockerSrc[1]);
-                console.log(`------`);
-
-                let stockerVariable=element.nextElementSibling
-                tabStockerVariable.push(stockerVariable)
-                console.log(tabStockerVariable[0]);
-                console.log(tabStockerVariable[1]);
-                console.log(`------`);
-
-
-
-                let stockerDos=element
-                tabStockerDos.push(stockerDos)
-                console.log(tabStockerDos[0]);
-                console.log(tabStockerDos[1]);
-                console.log(`------`);
-
-
-    
-                compteur++
-            }
-
-            if(compteur==2){
-                stop=true
-
-
-
-                // base des bases ici 
-                // if(tabStockerSrc[0]==tabStockerSrc[1]){
-                //     // element.nextElementSibling.style.border="green 2px solid"
-                //     tabStockerVariable[0].style.border="green 2px solid"
-                //     tabStockerVariable[1].style.border="green 2px solid"
-                // } else {
-
-                //     tabStockerVariable[0].style.border="red 2px solid"
-                //     tabStockerVariable[1].style.border="red 2px solid"
-                // 
-                if(tabStockerSrc[0]==tabStockerSrc[1]){
-                    // element.nextElementSibling.style.border="green 2px solid"
-                    tabStockerVariable[0].style.border="green 2px solid"
-                    tabStockerVariable[1].style.border="green 2px solid"
-                } else {
-                    tabStockerVariable[0].style.border="red 2px solid"
-                    tabStockerVariable[1].style.border="red 2px solid"
-
-                    // setTimeout(() => {
-                    //     tabStockerVariable[0].src="./public/img/dosDeCarte.jpeg"
-                    //     tabStockerVariable[1].src="./public/img/dosDeCarte.jpeg"
-                    // }, "1000");
-                    // if(tabStockerVariable[0].style.border=="red 2px solid" && tabStockerVariable[1].style.border=="red 2px solid"){
-                    //     tabStockerVariable[0].src="./public/img/dosDeCarte.jpeg"
-                    //     tabStockerVariable[1].src="./public/img/dosDeCarte.jpeg"
-                    // }
-
-                    
-
-
-                    // console.log(`A partir de en dessous`);
-                    // tabStockerVariable[0].style.display="none"
-                    // console.log(tabStockerVariable[0]);
-                    // tabStockerVariable[1].style.display="none"
-                    // console.log(tabStockerVariable[1]);
-                    // tabStockerDos[0].style.display="flex"
-                    // console.log(tabStockerDos[0]);
-                    // tabStockerDos[1].style.display="flex"
-                    // console.log(tabStockerDos[1]);
-
-                    // console.log(`A partir de en dessous`);
-                    // tabStockerVariable[0].style.display="none"
-                    // console.log(tabStockerVariable[0]);
-                    // tabStockerVariable[1].style.display="none"
-                    // console.log(tabStockerVariable[1]);
-                    // tabStockerDos[0].style.display="flex"
-                    // console.log(tabStockerDos[0]);
-                    // tabStockerDos[1].style.display="flex"
-                    // console.log(tabStockerDos[1]);
-
-                    // console.log(`A partir de en dessous`);
-                    // tabStockerVariable[0].style.display="none"
-                    // console.log(tabStockerVariable[0]);
-                    // tabStockerVariable[1].style.display="none"
-                    // console.log(tabStockerVariable[1]);
-                    // tabStockerDos[0].style.display="flex"
-                    // console.log(tabStockerDos[0]);
-                    // tabStockerDos[1].style.display="flex"
-                    // console.log(tabStockerDos[1]);
-
-
-                    // // teste 1
-                    // tabStockerVariable[0].setAttribute("class", "none")
-                    // tabStockerVariable[1].setAttribute("class", "none")
-                    // console.log(tabStockerVariable[0]);
-                    // console.log(tabStockerVariable[1]);
-
-                    // tabStockerDos[0].setAttribute("class", "flex")
-                    // tabStockerDos[1].setAttribute("class", "flex")
-                    // console.log(tabStockerDos[0]);
-                    // console.log(tabStockerDos[1]);
-                    // // fin teste 1
-
-
-
-                    // if(tabStockerVariable[0].style.border=="red 2px solid" && tabStockerVariable[1].style.border=="red 2px solid"){
-                    //     tabStockerVariable[0].style.display="none"
-                    //     tabStockerVariable[1].style.display="none"
-    
-                    //     tabStockerDos[0].style.display="flex"
-                    //     tabStockerDos[1].style.display="flex"
-                    // }
-                } 
-                tabStockerSrc=[]
-                tabStockerVariable=[]
-                tabStockerDos=[]
-                stop=false
-                compteur = 0
-
-
-
-            }
-
-    
-
-        })
-
-    });
-
 })
 
+dosDeCarteFacileAll.forEach(element => {
 
-                    // setTimeout(() => {
-                    //     alert(`je fct ok ?`)
-                    // }, "1000");
+    element.addEventListener("click", function () {
+
+        if (stop == false) {
+            console.log(element);
+            element.style.display = "none"
+            console.log(element);
+            console.log(element);
+            element.nextElementSibling.style.display = "flex"
+
+            
+            let stockerSrc = element.nextElementSibling.src
+            tabStockerSrc.push(stockerSrc)
+            // console.log(tabStockerSrc[0]);
+            // console.log(tabStockerSrc[1]);
+            console.log(`------`);
+
+            let stockerVariable = element.nextElementSibling
+            tabStockerVariable.push(stockerVariable)
+            // console.log(tabStockerVariable[0]);
+            // console.log(tabStockerVariable[1]);
+            console.log(`------`);
+
+            console.log(tabStockerVariable);
+            console.log(tabStockerSrc);
+
+            compteur++
+
+        }
+
+
+        if (compteur == 2) {
+            stop = true
+
+            if (tabStockerSrc[0] == tabStockerSrc[1]) {
+                console.log("juste");
+                tabStockerVariable.forEach(element => {
+                    element.style.border = "#ffe433 20px solid"
+                });
+                compteurRécup6+=2
+                if(compteurRécup6==dosDeCarteFacileAll.length){
+                    setTimeout(()=>{
+                        dosDeCarteFacileAll.forEach(element => {
+                            element.style.display = "flex"
+                        });
+                        devantDeCarteFacileAll.forEach(element => {
+                            element.style.display = "none"
+                            element.style.border = ""
+                        });
+                        compteurRécup6 = 0
+                        tabStockerSrc = []
+                        tabStockerVariable = []
+                        stop = false
+                        compteur = 0
+                        difficuterDeJeu.style.display = "flex"
+                    },1000)
+                }
+
+            } else {
+                tabStockerVariable.forEach(element => {
+                    // element.style.display = 'flex'
+                    element.style.border = "#a70b0b 20px solid"
+                    console.log(element);
+                    console.log(element.previousElementSibling);
+                    setTimeout(() => {
+                        element.previousElementSibling.style.display = 'flex'
+                        element.style.display = 'none'
+                    }, 1000);
+                });
+
+            }
+            tabStockerSrc = []
+            console.log("test");
+            console.log(tabStockerSrc);
+            console.log(tabStockerVariable);
+            tabStockerVariable = []
+            compteur = 0
+            setTimeout(()=>{
+                stop = false
+            },1000)
+            
+        }
+
+    })
+
+
+});
+
+
+
+
+
+
+
